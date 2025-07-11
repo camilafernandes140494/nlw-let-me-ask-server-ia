004a80430814b943dd0b4c6f0f95d35dede3e2c1
Camila <camilaferna140494@gmail.com>
2025-07-10 21:57:14 -0300

    feat: register HTTP routes for creating rooms and fetching questions

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,9 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
+import { postRoomsQuestions } from './http/routes/create-questions.ts';
+import { postRoomsRoute } from './http/routes/create-rooms.ts';
+import { getRoomsQuestions } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -23,5 +26,8 @@ app.get('/health', () => {
 });
 
 app.register(getRoomsRoute);
+app.register(postRoomsRoute);
+app.register(getRoomsQuestions);
+app.register(postRoomsQuestions);
 
 app.listen({ port: env.PORT });
