64d3a132058e020334adc420af5fb6e1ecd43294
Camila <camilaferna140494@gmail.com>
2025-07-10 21:57:53 -0300

    feat: implement route to create new rooms

---
+++ src/http/routes/create-rooms.ts
@@ -0,0 +1,40 @@
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
+import { z } from 'zod/v4';
+import { db } from '../../db/connection.ts';
+import { schema } from '../../db/schema/index.ts';
+
+export const postRoomsRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    '/rooms',
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(1, 'Name is required'),
+          description: z.string().optional(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, description } = request.body;
+
+      const result = await db
+        .insert(schema.rooms)
+        .values({
+          name,
+          description,
+        })
+        .returning();
+
+      const insertedRoom = result[0];
+
+      if (!insertedRoom) {
+        reply.status(500).send({ error: 'Failed to create room' });
+        return;
+      }
+      return reply.status(201).send({
+        message: 'Room created successfully',
+        roomId: insertedRoom.id,
+      });
+    }
+  );
+};
