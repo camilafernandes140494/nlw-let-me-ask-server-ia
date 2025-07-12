b2bbb9d7e212ef725766710815a65d3f9408428e
Camila <camilaferna140494@gmail.com>
2025-07-12 16:27:36 -0300

    feat: add embeddings generation and audio chunk retrieval to question creation

--- src/http/routes/create-questions.ts
+++ src/http/routes/create-questions.ts
@@ -1,8 +1,9 @@
+import { and, eq, sql } from 'drizzle-orm';
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import { z } from 'zod/v4';
 import { db } from '../../db/connection.ts';
 import { schema } from '../../db/schema/index.ts';
-
+import { generateEmbeddings } from '../../services/germini.ts';
 export const postRoomsQuestions: FastifyPluginCallbackZod = (app) => {
   app.post(
     '/rooms/:roomId/questions',
@@ -19,6 +20,36 @@ export const postRoomsQuestions: FastifyPluginCallbackZod = (app) => {
     async (request, reply) => {
       const { roomId } = request.params;
       const { questions } = request.body;
+
+      const embeddings = await generateEmbeddings(questions);
+
+      const embeddingsAsString = `[${embeddings.join(',')}]`;
+
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3);
+
+      let answer: string | null = null;
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription);
+
+        answer = await generateAnswer(question, transcriptions);
+      }
 
       const result = await db
         .insert(schema.questions)
